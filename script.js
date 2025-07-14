fc5c8590d552e690366c08757cd990d539a674f6
mdsayemislam07 <109275826+mdsayemislam07@users.noreply.github.com>
2025-07-14 20:48:45 +0600

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ const api_url = "https://api.themoviedb.org/3";
 const img_path = "https://image.tmdb.org/t/p/w500";
 const drive_api = "https://script.google.com/macros/s/AKfycbxK21Hi11PyGJ96T3_YhGWBOQvkSTtAnWvQckUXofBemfLW_AFVM1w3fqAl4RceCBWB/exec";
 
-// Better cleaner function
-function cleanFileName(fileName) {
-  fileName = fileName.replace(/\.[^/.]+$/, ""); // Extension remove
-  fileName = fileName.replace(/[\._]/g, " ");   // . _ replace with space
-  fileName = fileName.replace(/\b(480p|720p|1080p|2160p|BluRay|WEBRip|HDRip|x264|x265|HEVC|AAC|MP4|MKV|Dual Audio|Hindi|Bangla|English|BDRip|BRRip|HDCAM|WEB-DL)\b/gi, "");
-  fileName = fileName.replace(/\b(19|20)\d{2}\b/g, ""); // Year Remove
-  return fileName.trim();
+// Cleaner + Year Extractor
+function extractTitleAndYear(fileName) {
+  let name = fileName.replace(/\.[^/.]+$/, ""); // Remove extension
+  name = name.replace(/[\._]/g, " ");           // . and _ to space
+
+  let yearMatch = name.match(/\b(19|20)\d{2}\b/);
+  let year = yearMatch ? yearMatch[0] : null;
+
+  // Remove year and brackets if present
+  name = name.replace(/[\(\)\[\]]/g, "");        // Remove brackets
+  name = name.replace(/\b(19|20)\d{2}\b/, "");   // Remove year
+  name = name.replace(/\b(480p|720p|1080p|2160p|BluRay|WEBRip|HDRip|x264|x265|HEVC|AAC|MP4|MKV|Dual Audio|Hindi|Bangla|English|BDRip|BRRip|HDCAM|WEB-DL)\b/gi, "");
+
+  return {
+    title: name.trim(),
+    year: year
+  };
 }
 
-// Create movie card safely
 function createMovieCard(poster, title) {
   const card = document.createElement("div");
   card.className = "movie";
@@ -39,14 +48,17 @@ async function fetchMovies() {
   const files = await res.json();
 
   const promises = files.map(async (file) => {
-    const cleanName = cleanFileName(file.name);
-    const query = encodeURIComponent(cleanName);
+    const { title, year } = extractTitleAndYear(file.name);
+    const query = encodeURIComponent(title);
 
     let poster = file.url; // fallback to drive image
-    let title = file.name;
+    let finalTitle = file.name;
 
-    // Try Movie First
     let tmdbUrl = `${api_url}/search/movie?api_key=${client_key}&query=${query}`;
+    if (year) {
+      tmdbUrl += `&year=${year}`;
+    }
+
     let tmdbRes = await fetch(tmdbUrl);
     let tmdbData = await tmdbRes.json();
 
@@ -55,9 +67,9 @@ async function fetchMovies() {
       if (movie.poster_path) {
         poster = img_path + movie.poster_path;
       }
-      title = movie.title;
+      finalTitle = movie.title;
     } else {
-      // Try TV if movie fails
+      // Try TV Show if movie fails
       tmdbUrl = `${api_url}/search/tv?api_key=${client_key}&query=${query}`;
       tmdbRes = await fetch(tmdbUrl);
       tmdbData = await tmdbRes.json();
@@ -67,15 +79,15 @@ async function fetchMovies() {
         if (show.poster_path) {
           poster = img_path + show.poster_path;
         }
-        title = show.name;
+        finalTitle = show.name;
       }
     }
 
-    const card = createMovieCard(poster, title);
+    const card = createMovieCard(poster, finalTitle);
     container.appendChild(card);
   });
 
-  await Promise.all(promises); // Parallel Load
+  await Promise.all(promises);
 }
 
 fetchMovies();
