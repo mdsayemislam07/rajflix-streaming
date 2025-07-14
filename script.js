a7c9b92da96975f49a0b3bcff47d85e4d41ec97c
mdsayemislam07 <109275826+mdsayemislam07@users.noreply.github.com>
2025-07-14 20:39:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,53 +4,50 @@ const img_path = "https://image.tmdb.org/t/p/w500";
 
 const drive_api = "https://script.google.com/macros/s/AKfycbxK21Hi11PyGJ96T3_YhGWBOQvkSTtAnWvQckUXofBemfLW_AFVM1w3fqAl4RceCBWB/exec";
 
+// Clean function (Best Match)
 function cleanFileName(fileName) {
-  // Extension Remove
-  fileName = fileName.replace(/\.[^/.]+$/, "");
-
-  // Dot -> Space
-  fileName = fileName.replace(/\./g, " ");
-
-  // Resolution, Year, Extra Info Remove
-  fileName = fileName.replace(/\b(480p|720p|1080p|2160p|BluRay|WEBRip|WEB-DL|HDRip|x264|x265|HEVC|AAC|MP4|MKV|Hindi|English|Dual Audio|Bangla|BDRip|BRRip|HDCAM)\b/gi, "");
-  
-  // Year Remove
-  fileName = fileName.replace(/\b(19|20)\d{2}\b/g, "");
-
-  // Trim Extra Spaces
+  fileName = fileName.replace(/\.[^/.]+$/, ""); // Extension remove
+  fileName = fileName.replace(/[\._]/g, " ");   // . _ replace with space
+  fileName = fileName.replace(/\b(480p|720p|1080p|2160p|BluRay|WEBRip|HDRip|x264|x265|HEVC|AAC|MP4|MKV|Dual Audio|Hindi|Bangla|English|BDRip|BRRip|HDCAM|WEB-DL)\b/gi, "");
+  fileName = fileName.replace(/\b(19|20)\d{2}\b/g, ""); // Year Remove
   return fileName.trim();
 }
 
 async function fetchFiles() {
   const res = await fetch(drive_api);
   const files = await res.json();
-  
+
   for (const file of files) {
     const cleanName = cleanFileName(file.name);
     const query = encodeURIComponent(cleanName);
     const tmdb_url = `${api_url}/search/movie?api_key=${client_key}&query=${query}`;
     
-    const tmdb_res = await fetch(tmdb_url);
-    const tmdb_data = await tmdb_res.json();
-    
-    if (tmdb_data.results && tmdb_data.results.length > 0) {
-      const movie = tmdb_data.results[0];
-      const poster = movie.poster_path ? img_path + movie.poster_path : file.url;
-      
+    try {
+      const tmdb_res = await fetch(tmdb_url);
+      const tmdb_data = await tmdb_res.json();
+
+      let poster = "";
+      let title = "";
+
+      if (tmdb_data.results && tmdb_data.results.length > 0) {
+        const movie = tmdb_data.results[0];
+        poster = movie.poster_path ? img_path + movie.poster_path : file.url;
+        title = movie.title;
+      } else {
+        // No TMDB Match, fallback to Drive image
+        poster = file.url;
+        title = file.name;
+      }
+
       document.getElementById("movies").innerHTML += `
         <div class="movie">
-          <img src="${poster}" alt="${movie.title}">
-          <div class="movie-title">${movie.title}</div>
+          <img src="${poster}" alt="${title}">
+          <div class="movie-title">${title}</div>
         </div>
       `;
-    } else {
-      // যদি মেল না খায়, Drive preview দেখাবে
-      document.getElementById("movies").innerHTML += `
-        <div class="movie">
-          <img src="${file.url}" alt="${file.name}">
-          <div class="movie-title">${file.name}</div>
-        </div>
-      `;
+
+    } catch (err) {
+      console.error("Error fetching TMDB:", err);
     }
   }
 }
